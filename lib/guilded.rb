1847e298ddb017a512dfb1077ff1c54d9261008a
Jason Harrelson <jason@lookforwardenterprises.com>
2009-03-24 14:34:17 -0500

    Release 0.1.6.

--- lib/guilded.rb
+++ lib/guilded.rb
@@ -71,7 +71,7 @@ require 'guilded/rails/active_record/human_attribute_override'
 #   <%= g_load_alerter :skin => 'blueish', :id => 'load_alerter' %>
 #
 module Guilded
-  VERSION = '0.1.5'
+  VERSION = '0.1.6'
 end
 
 ActionView::Base.send( :include, Guilded::Rails::ViewHelpers ) if defined?( ActionView )
