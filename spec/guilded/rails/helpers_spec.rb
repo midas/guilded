a715dc9d6e6cb7d659001adc6110dd1d7c1fe222
Jason Harrelson <jason@lookforwardenterprises.com>
2009-03-26 13:02:31 -0500

    Everything is tested and working as expected with REST path helper.

--- spec/guilded/rails/helpers_spec.rb
+++ spec/guilded/rails/helpers_spec.rb
@@ -438,7 +438,37 @@ describe "Guilded::Rails::Helpers" do
   end
   
   it "should resolve shallow nested REST paths given ActiveRecord object and scoped by multiple object array and namespace symbol" do
-    path_helpers = Guilded::Rails::Helpers.resolve_rest_path_helpers( @thing, :scoped_by => [@owner, @parent], :namespace => :admin, :shallow => true )
+    path_helpers = Guilded::Rails::Helpers.resolve_rest_path_helpers( @thing, :scoped_by => [@owner, @parent], :namespace => :admin, 
+      :shallow => true )
+    path_helpers[:index_rest_helper].should eql( 'admin_owner_parent_things_path' )
+    path_helpers[:index_rest_args].should eql( [@owner, @parent] )
+    path_helpers[:show_rest_helper].should eql( 'admin_owner_thing_path' )
+    path_helpers[:show_rest_args].should eql( [@owner] )
+    path_helpers[:new_rest_helper].should eql( 'new_admin_owner_parent_thing_path' )
+    path_helpers[:new_rest_args].should eql( [@owner, @parent] )
+    path_helpers[:edit_rest_helper].should eql( 'edit_admin_owner_thing_path' )
+    path_helpers[:edit_rest_args].should eql( [@owner] )
+    path_helpers[:delete_rest_helper].should eql( 'delete_admin_owner_thing_path' )
+    path_helpers[:delete_rest_args].should eql( [@owner] )
+  end
+  
+  it "should resolve single level REST paths given a polymorphic ActiveRecord object" do
+    path_helpers = Guilded::Rails::Helpers.resolve_rest_path_helpers( @lookup, :polymorphic_type => 'Thing', :polymorphic_as => :lookupable )
+    path_helpers[:index_rest_helper].should eql( 'things_path' )
+    path_helpers[:index_rest_args].should eql( [] )
+    path_helpers[:show_rest_helper].should eql( 'thing_path' )
+    path_helpers[:show_rest_args].should eql( [] )
+    path_helpers[:new_rest_helper].should eql( 'new_thing_path' )
+    path_helpers[:new_rest_args].should eql( [] )
+    path_helpers[:edit_rest_helper].should eql( 'edit_thing_path' )
+    path_helpers[:edit_rest_args].should eql( [] )
+    path_helpers[:delete_rest_helper].should eql( 'delete_thing_path' )
+    path_helpers[:delete_rest_args].should eql( [] )
+  end
+  
+  it "should resolve shallow nested REST paths given a polymorphic ActiveRecord object and scoped by multiple object array and namespace symbol" do
+    path_helpers = Guilded::Rails::Helpers.resolve_rest_path_helpers( @looku, :scoped_by => [@owner, @parent], :namespace => :admin, 
+      :shallow => true, :polymorphic_type => 'Thing', :polymorphic_as => :lookupable )
     path_helpers[:index_rest_helper].should eql( 'admin_owner_parent_things_path' )
     path_helpers[:index_rest_args].should eql( [@owner, @parent] )
     path_helpers[:show_rest_helper].should eql( 'admin_owner_thing_path' )
