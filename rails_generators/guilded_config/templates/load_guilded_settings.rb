63c629e4c7401120a1683ff689d68377452ddaf2
Jason Harrelson <jason@lookforwardenterprises.com>
2010-04-16 20:14:50 -0500

    Updated the Rails Guilded initializer template to use Rails.root.

--- rails_generators/guilded_config/templates/load_guilded_settings.rb
+++ rails_generators/guilded_config/templates/load_guilded_settings.rb
@@ -2,11 +2,11 @@
 # on this one in order to work in different environments.
 #
 GUILDED_CONFIG = {
-  :js_path => "#{RAILS_ROOT}/public/javascripts/",
+  :js_path => "#{Rails.root}/public/javascripts/",
   :js_folder => "", # from the js_path
   :jquery_js => "jquery/jquery-1.3.2.min.js", # from the js_path
   :mootools_js => "mootools/mootools-1.2.3.min.js", # from the js_path
-  :css_path => "#{RAILS_ROOT}/public/stylesheets/",
+  :css_path => "#{Rails.root}/public/stylesheets/",
   :css_folder => "guilded/", # from the css_path
   :reset_css => "reset-min.css", # from the css_path
   :environment => Rails.env
